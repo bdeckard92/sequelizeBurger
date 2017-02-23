12a19b7b7950cb616a3daa56ad4b4710b021623f
Bob Deckard <bdeckard92@gmail.com>
2017-02-23 15:34:33 -0600

    not sequelized yet..

---
+++ burgers_db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Plain and Dry');
+INSERT INTO burgers (name) VALUES ('Double Cheese');
+INSERT INTO burgers (name, sleepy) VALUES ('Avacado');
+INSERT INTO burgers (name, sleepy) VALUES ('Royal With Cheese';
+INSERT INTO burgers (name, sleepy) VALUES ('Buffalo');
+INSERT INTO burgers (name) VALUES ('Ostritch');
